2064c2f9cff581583808a83ae43d9266a8a4baaa
jccaldw1 <jacobccaldwell@outlook.com>
2023-12-20 20:22:29 -0700

    fix update method

--- Components/Services/ChristmasPresentService.cs
+++ Components/Services/ChristmasPresentService.cs
@@ -50,7 +50,7 @@ public class ChristmasPresentService(IDbContextFactory<UsersContext> usersContex
     {
         using var usersContext = usersContextFactory.CreateDbContext();
 
-        var presentToUpdate = usersContext.ChristmasPresents.AsEnumerable().FirstOrDefault(present);
+        var presentToUpdate = usersContext.ChristmasPresents.AsEnumerable().FirstOrDefault(presentToLookFor => presentToLookFor._id == present._id);
 
         presentToUpdate.gotten = gotten;
 
