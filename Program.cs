6a05945789ae850cada1e6b6b348fc1ff29785d1
jccaldw1 <jacobccaldwell@outlook.com>
2023-12-28 15:02:56 -0700

    add azure integration and bootstrap styling

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Azure.Identity;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Azure;
 using Christmas.Components;
 using Christmas.Components.Container;
 using Christmas.Components.MongoDb.Contexts;
@@ -6,6 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var BlobStorageUri = builder.Configuration["AzureURIs:BlobStorage"];
+var KeyVaultURI = builder.Configuration["AzureURIs:KeyVault"];
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -24,6 +30,8 @@ builder.Services.AddScoped<SelectedUserContainer>();
 
 var authenticationBuilder = builder.Services.AddAuthentication("Cookies");
 authenticationBuilder.AddCookie("Cookies");
+
+builder.Services.AddAzureClientsCore();
 
 var app = builder.Build();
 
